4cf9eaf3d2c81bac872e90aedf349eecd87c7ca2
Hari Krishnan <88133815+harizahh@users.noreply.github.com>
2025-04-24 14:53:23 -0400

    Add files via upload

---
+++ testcode1.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.decomposition import PCA
+
+rng = np.random.RandomState(0)
+n_samples = 500
+cov = [[3, 3], [3, 4]]
+X = rng.multivariate_normal(mean=[0, 0], cov=cov, size=n_samples)
+pca = PCA(n_components=2).fit(X)
+
+
+plt.scatter(X[:, 0], X[:, 1], alpha=0.3, label="samples")
+for i, (comp, var) in enumerate(zip(pca.components_, pca.explained_variance_)):
+    comp = comp * var  # scale component by its variance explanation power
+    plt.plot(
+        [0, comp[0]],
+        [0, comp[1]],
+        label=f"Component {i}",
+        linewidth=5,
+        color=f"C{i + 2}",
+    )
+plt.gca().set(
+    aspect="equal",
+    title="2-dimensional dataset with principal components",
+    xlabel="first feature",
+    ylabel="second feature",
+)
+plt.legend()
+# plt.show()
+plt.savefig("plot1.png", dpi=300)
+
+y = X.dot(pca.components_[1]) + rng.normal(size=n_samples) / 2
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+axes[0].scatter(X.dot(pca.components_[0]), y, alpha=0.3)
+axes[0].set(xlabel="Projected data onto first PCA component", ylabel="y")
+axes[1].scatter(X.dot(pca.components_[1]), y, alpha=0.3)
+axes[1].set(xlabel="Projected data onto second PCA component", ylabel="y")
+plt.tight_layout()
+# plt.show()
+plt.savefig("plot2.png", dpi=300)
