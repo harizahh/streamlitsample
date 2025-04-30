41daa6ed29005f3f6ac36890afdb18daf4f9003f
Hari Krishnan <88133815+harizahh@users.noreply.github.com>
2025-04-29 23:00:11 -0400

    Update testcode1.py

--- testcode1.py
+++ testcode1.py
@@ -1,9 +1,7 @@
 import streamlit as st
-st.title("Data Analysis")
-
-st.write("Yes it really works")
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit_pca as sns
 
 from sklearn.decomposition import PCA
 
@@ -14,7 +12,8 @@ X = rng.multivariate_normal(mean=[0, 0], cov=cov, size=n_samples)
 pca = PCA(n_components=2).fit(X)
 
 
-plt.scatter(X[:, 0], X[:, 1], alpha=0.3, label="samples")
+# plt.scatter(X[:, 0], X[:, 1], alpha=0.3, label="samples")
+st.scatter_chart(X, x=None, y=None, color=None, size=None, height=None, width=None) # changed line
 for i, (comp, var) in enumerate(zip(pca.components_, pca.explained_variance_)):
     comp = comp * var  # scale component by its variance explanation power
     plt.plot(
@@ -32,7 +31,7 @@ plt.gca().set(
 )
 plt.legend()
 # plt.show()
-#plt.savefig("plot1.png", dpi=300)
+plt.savefig("plot1.png", dpi=300)
 
 y = X.dot(pca.components_[1]) + rng.normal(size=n_samples) / 2
 
@@ -44,4 +43,4 @@ axes[1].scatter(X.dot(pca.components_[1]), y, alpha=0.3)
 axes[1].set(xlabel="Projected data onto second PCA component", ylabel="y")
 plt.tight_layout()
 # plt.show()
-#plt.savefig("plot2.png", dpi=300)
+plt.savefig("plot2.png", dpi=300)
